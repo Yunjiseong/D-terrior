f6a7035d20ebab5940c9a539162fda519eef5706
Busterz3682 <87509072+Busterz3682@users.noreply.github.com>
2021-09-15 14:20:11 +0900

    login testing

---
+++ Project/MyHome/src/main/java/com/spring/myWeb/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.spring.myWeb.controller;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.myWeb.command.UserVO;
+import com.spring.myWeb.user.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+
+	//회원가입페이지(일반)이동
+	@GetMapping("normalJoinPage")
+	public String joinPageNormal() {
+		return "user/normalJoin";
+	}
+
+	//회원가입페이지(전문가)이동
+	@GetMapping("proJoinPage")
+	public String joinPagePro() {
+		return "user/proJoin";
+	}
+
+	//회원가입요청
+	@PostMapping("/userJoin")
+	public String join(UserVO vo) {
+		System.out.println("회원가입요청");
+		service.userJoin(vo);
+		return "user/loginPage";
+	}
+
+	//회원정보수정요청
+	@PostMapping("/userUpdate")
+	public void userUpdate(UserVO vo) {
+		System.out.println("회원정보수정요청");
+		service.userUpdate(vo);
+	}
+
+	//회원탈퇴요청
+	@PostMapping("/userDelete")
+	public void userDelete(@Param("id")String id, @Param("pw")String pw) {
+		System.out.println("회원탈퇴요청");
+		service.userDelete(id, pw);
+	}
+
+	//로그인페이지이동
+	@GetMapping("/userLoginPage")
+	public String loginPage() {
+		return "user/loginPage";
+	}
+	
+}
---
+++ Project/MyHome/src/main/java/com/spring/myWeb/user/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package com.spring.myWeb.user.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.spring.myWeb.command.UserVO;
+
+public interface UserMapper {
+
+	//CRUD 기능 메서드
+	//회원등록
+	void userJoin(UserVO vo);
+	
+	//회원정보 불러오기
+	UserVO userInfo(String id);
+	
+	//회원정보 수정
+	void userUpdate(UserVO vo);
+	
+	//회원탈퇴
+	void userDelete(@Param("id") String id, @Param("pw") String pw);
+	
+	//로그인
+	UserVO login(String id, String pw);
+}
